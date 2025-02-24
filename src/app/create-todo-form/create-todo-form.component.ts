a9f3a961dd206bcd37e9e11ed3b6618763599d13
AbuAsiay <AbuAsiay@mail.com>
2025-02-24 23:50:18 +0200

    12 lesson Angular Material

--- src/app/create-todo-form/create-todo-form.component.ts
+++ src/app/create-todo-form/create-todo-form.component.ts
@@ -1,6 +1,11 @@
 import { NgIf } from "@angular/common";
 import { Component, EventEmitter, Output } from "@angular/core";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @Component({
@@ -8,30 +13,34 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angula
     templateUrl: './create-todo-form.component.html',
     styleUrl: './create-todo-form.component.scss',
     standalone: true,
-    imports: [ReactiveFormsModule, NgIf]
+    imports: [ReactiveFormsModule, NgIf,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatIconModule],
 })
 export class CreateTodoFormComponent {
     @Output()
     createTodo = new EventEmitter();
-    
+
     public form = new FormGroup({
-        title: new FormControl('', [Validators.required,Validators.minLength(2)]),
+        title: new FormControl('', [Validators.required, Validators.minLength(2)]),
         userId: new FormControl('', [Validators.required, Validators.minLength(1)]),
         completed: new FormControl('', [Validators.required, Validators.minLength(2)]),
     });
-    
+
     // сonstructor() { //constructor нужен для передачи сервисов в компонент
     // }
-        
-    
-    
+
+
+
 
     public submitForm(): void {
         this.createTodo.emit(this.form.value);
         this.form.reset();
         console.log('TODO IS CREATED',);
     }
-    
+
     constructor() {
         this.form.valueChanges.subscribe(
             (value) => {
--- src/app/create-user-form/create-user-form.component.ts
+++ src/app/create-user-form/create-user-form.component.ts
@@ -1,32 +1,41 @@
 import { NgIf } from "@angular/common";
 import { Component, EventEmitter, Output } from "@angular/core";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
     selector: 'app-create-user-form',
     templateUrl: './create-user-form.component.html',
     styleUrl: './create-user-form.component.scss',
     standalone: true,
-    imports: [ReactiveFormsModule, NgIf],
-    
+    imports: [ReactiveFormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatIconModule,],
+
 })
 export class CreateUserFormComponent {
     @Output()
     createUser = new EventEmitter();
-    
+
     public form = new FormGroup({
-        name: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        name: new FormControl('', [Validators.required, Validators.minLength(2)]),
         email: new FormControl('', [Validators.required, Validators.email]),
         website: new FormControl('', [Validators.required, Validators.minLength(2)]),
         companyName: new FormControl('', [Validators.required, Validators.minLength(2)]),
     });
-formTodo: any;
-    
-    public sumbitForm(): void{
-    this.createUser.emit(this.form.value);
-    this.form.reset();
+    formTodo: any;
+
+    public sumbitForm(): void {
+        this.createUser.emit(this.form.value);
+        this.form.reset();
     }
-    
+
     constructor() {
         this.form.valueChanges.subscribe(
             (value) => {
